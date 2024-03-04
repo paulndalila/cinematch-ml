f8af5deb6443e917546f7a505c6a48bb53dd0ab6
Paul Ndalila <paulndalila001@gmail.com>
2024-03-04 16:19:25 +0300

    CineMatch Frontend, ML and API integration

--- movie-api/app.py
+++ movie-api/app.py
@@ -5,19 +5,33 @@ import pandas as pd
 import difflib
 import uvicorn
 from joblib import load
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-movies_data = pd.read_csv('movies.csv')
+
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="statics"), name="static")
 
 similarity = load('similariity.joblib')
+movies_data = pd.read_csv('movies.csv')
 
-def sort_similar_movies(movie_name):
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/process_form")
+async def process_form(request: Request, movie_name: str = Form(...)):
     i = 1
     movies=[]
+    close_matches_with_overviews = []
     
     list_of_all_titles = movies_data['title'].tolist()
     find_close_match = difflib.get_close_matches(movie_name, list_of_all_titles)
+    
+    for close_match in find_close_match:
+        overview = movies_data[movies_data.title == close_match]['overview'].values[0]
+        close_matches_with_overviews.append({"title": close_match, "overview": overview})
+        
     close_match = find_close_match[0]
     index_of_the_movie = movies_data[movies_data.title == close_match]['index'].values[0]
     similarity_score = list(enumerate(similarity[index_of_the_movie]))
@@ -26,24 +40,14 @@ def sort_similar_movies(movie_name):
     for movie in sorted_similar_movies:
       index = movie[0]
       title_from_index = movies_data[movies_data.index==index]['title'].values[0]
+      overview_from_index = movies_data[movies_data.index == index]['overview'].values[0]
       if (i<30):
-        movies.append(title_from_index)
+        movies.append({"title":title_from_index, "overview": overview_from_index})
         i+=1
 
-    return movies
-
-@app.get("/ping")
-async def ping():
-    return "Hello, I am alive"
-
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-@app.post("/process_form")
-async def process_form(movie_name: str = Form(...)):
-    similar_movies = sort_similar_movies(movie_name)
-    return similar_movies
+    similar_movies = movies
+    #return similar_movies
+    return templates.TemplateResponse("results.html", {"request": request, "movie_name": movie_name, "similar_movies": list(similar_movies), "close_match": close_matches_with_overviews})
 
 if __name__ == "__main__": 
     uvicorn.run(app, host='localhost', port=8000)
